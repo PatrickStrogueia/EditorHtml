fe1f984f7658e5e21615e4b372c6caa81ec0ca7f
Patrick Strogueia <pstrogueia@fazenda.ms.gov.br>
2024-02-01 21:19:27 -0400

    Finalizando o projeto

--- obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43454c09d0cf4b1a157ebc7462748b3978858ca5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
